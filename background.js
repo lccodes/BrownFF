33b289cf9ca47b95c7773a089618213f1ad368d3
lccodes <luke_camery@brown.edu>
2014-07-30 10:15:24 -0400

    Survey feature

--- background.js
+++ background.js
@@ -1,21 +1,23 @@
 //What happens when you visit NFL.com
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
-    var deny = false;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange=function(){
-      if (xmlhttp.readyState==4 && xmlhttp.status==200){
-        var where = xmlhttp.responseText.indexOf(localStorage.username + ",");
-        if(where != -1 && ("f" == xmlhttp.responseText.substr(where+10, 1))){
-          //Redirect if they need to fill out the survey
-          deny = true;
+    if(info.url.indexOf("teamlog") == -1){
+      var deny = false;
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange=function(){
+        if (xmlhttp.readyState==4 && xmlhttp.status==200){
+          var where = xmlhttp.responseText.indexOf(localStorage.username + ",");
+          if(where != -1 && ("t" == xmlhttp.responseText.substr(where+10, 1))){
+            //Redirect if they need to fill out the survey
+            deny = true;
+          }
         }
       }
-    }
-    xmlhttp.open("GET","http://jack.cs.brown.edu/didYou.txt",false);
-    xmlhttp.send();
-    if(deny){
-      return {redirectUrl: "http://jack.cs.brown.edu/survey"};
+      xmlhttp.open("GET","http://jack.cs.brown.edu/didYou.txt",false);
+      xmlhttp.send();
+      if(deny){
+        return {redirectUrl: "http://jack.cs.brown.edu/survey"};
+      }
     }
   },
   // filters
