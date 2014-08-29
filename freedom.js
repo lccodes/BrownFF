4a5c03b8eeabfa91de59d09e2eafef50d16cc254
lccodes <luke_camery@brown.edu>
2014-08-29 17:09:00 -0400

    Update freedom.js

--- freedom.js
+++ freedom.js
@@ -15,32 +15,34 @@ function onLoad(){
 		//alert("Success!");
 		var all = document.getElementsByTagName("input");
 		var allow = true;
-			all[0].value = us;
-			//alert(obj2['username']);
+		all[0].value = us;
+		//alert(obj2['username']);
 		
-			for(var i = 0; i < all.length; i++){
-				if(all[i].value == ""){
-					allow = false;
-				}
+		for(var i = 0; i < all.length; i++){
+			if(all[i].value == ""){
+				allow = false;
 			}
-			if(allow){
-				//alert("Free!");
-				var xmlhttp = new XMLHttpRequest();
-				//alert(stringToBinary(obj['username']));
-	      			xmlhttp.open("POST","http://jack.cs.brown.edu/data.php",true);
-	      			xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	      			xmlhttp.send("type=free&username="+stringToBinary(us);
-	      			alert(stringToBinary(us));
-			}
+		}
+		//Check if they filled it out properly
+		if(allow){
+			//alert("Free!");
+			var xmlhttp = new XMLHttpRequest();
+      		xmlhttp.open("POST","http://jack.cs.brown.edu/data.php",true);
+      		xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+      		xmlhttp.send("type=free&username="+stringToBinary(us));
+      		//alert(stringToBinary(us));
+		}
 	});
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     chrome.storage.sync.get("username", function(obj){
 		us = obj['username'];
+		//alert("Added");
+		window.onload = onLoad;
+		//alert(us);
+		var first = document.getElementsByTagName("input");
+		first[0].value = us;
+		//alert(us);
     });
-
-	//alert("Added");
-	window.onload = onLoad;
-	//alert("sfsd");	
 });
